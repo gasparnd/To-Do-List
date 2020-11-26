a9dd89d5a910760f1c43dc7b370e41e0f71b8c19
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2020-11-26 07:44:42 -0300

    initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: path.resolve(__dirname, 'src/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/main.js',
+		publicPath: 'dist/'
+	},
+	devServer: {
+		contentBase: 'dist/',
+		open: true,
+		port: '9080'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader'
+				},
+				exclude: /node_modules/
+			}
+		]
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			inject: true,
+			template: './public/index.html',
+			filename: 'index.html'
+		})
+	]
+}
